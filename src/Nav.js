f06bcd0bcc42081c3bd386965a49b40d81c57911
Justin Caovan <jtcaovan@gmail.com>
2021-03-29 13:59:12 -0700

    Added components

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
         <nav>
             <h3>Logo</h3>
             <ul>
-                <li>About</li>
-                <li>Shop</li>
+                <Link to='/about'>
+                    <li>About</li>
+                </Link>
+                <Link to='/shop'>
+                    <li>Shop</li>
+                </Link>
+                <Link to='/cart'>
+                    <li>Cart</li>
+                </Link>
             </ul>
         </nav>
   );
--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './App'
 import About from './About'
 import Shop from './Shop'
+import Cart from './Cart'
 
 const Routes = () => {
     return (
-        <BrowserRouter>
+        <Router>
             <Switch>
                 <Route exact path='/' component={App} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/shop' component={Shop} />
+                <Route exact path='/cart' component={Cart} />
             </Switch>
-        </BrowserRouter>
+        </Router>
     )
 }
 
