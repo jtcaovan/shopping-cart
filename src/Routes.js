6e08aadec4f3e2622c0c474ece0f4edb0e2a0e89
Justin Caovan <jtcaovan@gmail.com>
2021-04-01 10:16:45 -0700

    Changed home picture and added footer

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './index.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from '.components/Nav'
-import App from '.components/App'
-import About from '.components/About'
-import Shop from '.components/Shop'
-import Cart from '.components/Cart'
+import Nav from './components/Nav'
+import App from './components/App'
+import About from './components/About'
+import Shop from './components/Shop'
+import Cart from './components/Cart'
+import Footer from './components/Footer'
 
 const Routes = () => {
     return (
@@ -16,6 +18,7 @@ const Routes = () => {
                 <Route exact path='/shop' component={Shop} />
                 <Route exact path='/cart' component={Cart} />
             </Switch>
+            <Footer />
         </Router>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <footer className=''>
+            Hello
+        </footer>
+    )
+}
+
+export default Footer
