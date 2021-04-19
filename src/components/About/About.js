1710f985ec9221901e58716667d48f0ab353db16
Justin Caovan <jtcaovan@gmail.com>
2021-04-18 23:53:27 -0700

    Added image for about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,17 @@
 import React from "react";
+import about from 'assets/about.jpg'
 
 const About = () => {
   return (
     <div>
-      <h1>Hello from About</h1>
+        <div className='block h-3/4 w-screen'>
+          <div className='fixed h-3/4 max-h-full w-full'>
+            <div className='bg-cover bg-fixed bg-no-repeat h-full' 
+            style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url(${about})` }}>
+            <h1 className='absolute top-1/2 left-1/2 inset-0 font-display text-white text-5xl m-auto z-10'>Who Are We?</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
