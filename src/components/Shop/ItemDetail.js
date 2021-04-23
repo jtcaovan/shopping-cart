3ce8c4f4ff33cfba903deac6620e0053a79c2651
Justin Caovan <jtcaovan@gmail.com>
2021-04-23 08:16:51 -0700

    Added plant description and styling

--- src/components/Shop/ItemDetail.js
+++ src/components/Shop/ItemDetail.js
@@ -27,10 +27,15 @@ function ItemDetail() {
                 <div className='h-4/5 w-10/12 my-12 mx-auto flex space-x-24 justify-between '>
                     <img className='' src={plant.img} alt=''/>
 
-                    <div className='w-1/3 flex-col space-y-8'>
-                        <div className='flex'>
+                    <div className='w-2/5 flex-col space-y-8'>
+                        <div className='flex justify-between '>
                             <h1 className='font-display text-5xl'>{plant.name}</h1>
-                            <p className='font-body'>{plant.price}</p>
+                            <p className='font-body text-xl'>{plant.price}</p>
+                        </div>
+
+                        <div>
+                            <p className='font-body'>The {plant.name} is a beautiful, statement piece plant that brings style and brightness to any indoor space.
+                            This plant requires <span className='font-semibold'>{plant.light}</span>.</p>
                         </div>
 
                         <div>
@@ -63,8 +68,14 @@ function ItemDetail() {
                             </div>
                         </div>
 
-                        <button className='w-full h-12 p-4 font-body text-sm bg-gray-800 bg-opacity-60 text-white rounded-r
-                            transition duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'>Add to Cart</button>
+                        <button className='w-full h-12 p-4 font-body text-sm bg-gray-900 bg-opacity-70 text-white rounded-r
+                            transition duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'>Add to Cart
+                        </button>
+                        <div className='flex justify-around'>
+                            <p className='w-24 font-body text-gray-800'>Made to order</p>
+                            <p className='w-24 font-body text-gray-800'>60 Day Guarantee</p>
+                            <p className='w-24 font-body text-gray-800'>24/7 Plant Help</p>
+                        </div>
                     </div>
                 </div>
                 <Footer />
