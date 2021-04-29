85d5a5c313431ec17a091a7b00cc80d3b10fc01f
Justin Caovan <jtcaovan@gmail.com>
2021-04-28 22:54:25 -0700

    Close cart on X icon click, need to figure out how to display when cart is clicked on navbar

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState }from "react";
 
 const Cart = () => {
+  const [open, setOpen] = useState('')
+
   return (
-    <div className='relative'>
-      <div className='absolute inset-0 bg-gray-700 bg-opacity-50 h-screen z-20'>
-        <div className='absolute inset-y-0 right-0 h-screen w-1/3 bg-white z-10'>
-          <div className='flex justify-between'>
-            <p className='text-5xl font-display'>Cart (0)</p>
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </div>
-          <div>
-            <p>Your cart is currently empty!</p>
-            <button className='h-12 p-4 w-28 font-body text-sm bg-gray-900 text-white rounded-r transition duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'>
-            Shop Now</button>          
+    <div className={open}>
+      <div className='relative'>
+        <div className='absolute inset-0 bg-gray-700 bg-opacity-50 h-screen z-20'>
+          <div className='absolute inset-y-0 right-0 h-screen w-1/3 bg-white z-10'>
+            <div className='flex justify-between'>
+              <p className='text-5xl font-display'>Cart (0)</p>
+              <svg onClick={() => setOpen('invisible')} className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </div>
+            <div>
+              <p>Your cart is currently empty!</p>
+              <button className='h-12 p-4 w-28 font-body text-sm bg-gray-900 text-white rounded-r transition duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'>
+              Shop Now</button>          
+            </div>
           </div>
         </div>
       </div>
