7dc48b507a4450d69440b73288eaed1245017a52
Justin Caovan <jtcaovan@gmail.com>
2021-05-11 22:28:29 -0700

    Edited about info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,10 +11,12 @@ const About = () => {
       <div className="relative bg-white min-h-0 h-auto">
         <div className='w-9/12 text-center m-auto'>
             <h1 className='font-display text-4xl m-12'>Our Humble Beginnings</h1>
-            <p className='font-body'>In 2017, Justin Caovan lived in a small, boring apartment in Long Beach, CA while attending university.
+            <p className='font-body leading-loose'>In 2017, Justin Caovan lived in a small, boring apartment in Long Beach, CA while attending university.
             His first year without having to share a room, he decided to spruce it up and make it his own. At a local swapmeet, he picked up a spider plant,
-            a golden pothos, and a couple of succulents. The new plants instantly brightened up his dull room and he was soon hooked on buying more for his room.
-            Over time, his plants outgrew his tiny room so he decided to trim them and give the clippings to his roommates. 
+            a golden pothos, and a couple of succulents. The new plants instantly brightened up his dull room and he was soon hooked.
+            Over time, his plants outgrew his tiny room so he decided to propogate, pot, and gift them to his friends and family. 
+            </p>
+            <p className='font-body leading-loose'>
             </p>
         </div>
       </div>
