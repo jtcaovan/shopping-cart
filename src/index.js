60c67920442bfc5848908c5c31bf5c32c4786f07
Justin Caovan <jtcaovan@gmail.com>
2021-05-17 21:20:00 -0700

    Added Provider for store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes'
+import {Provider} from "react-redux"
 import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
+  <Provider>
+    <React.StrictMode>
+      <Routes />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
