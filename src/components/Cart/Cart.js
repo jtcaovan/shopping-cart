f1a405d0ff93a1362252da5c3bd314151d5ea528
Justin Caovan <jtcaovan@gmail.com>
2021-05-18 21:39:35 -0700

    Adding store and reducers for cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,16 @@
 import React, { useState }from "react";
 
 const Cart = () => {
-  const [open, setOpen] = useState('')
-
   return (
-    <div className={open}>
+    <div>
       <div className='relative overflow-hidden'>
         <div className='absolute inset-0 bg-gray-700 bg-opacity-50 h-screen z-20'>
           <div className='absolute inset-y-0 right-0 h-screen w-1/3 bg-white z-10'>
             <div className='m-6 flex justify-between'>
               <p className='text-4xl font-display'>Cart <span className='text-lg'>(0 items)</span></p>
-              <svg onClick={() => setOpen('invisible')} className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              {/* <svg onClick={() => setOpen('invisible')} className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
+              </svg> */}
             </div>
             <div className='absolute font-body inset-y-1/2 inset-x-1/3 w-full h-24'>
               <p>Your cart is empty!</p>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes'
-import {Provider} from "react-redux"
+import { Provider } from 'react-redux'
+import store from './redux'
 import './index.css'
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <Routes />
     </React.StrictMode>
