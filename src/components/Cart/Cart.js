534533580da38095965f3ac2a03eea3005a9a026
Justin Caovan <jtcaovan@gmail.com>
2021-05-22 21:05:18 -0700

    Added toggle display for cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,34 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { hideCart } from '../Cart/cartSlice'
 
 const Cart = () => {
+  const display = useSelector((state) => state.cart.display)
+  const dispatch = useDispatch()
+
   return (
-    <div>
-      <div className='relative overflow-hidden'>
-        <div className='absolute inset-0 bg-gray-700 bg-opacity-50 h-screen z-20'>
-          <div className='absolute inset-y-0 right-0 h-screen w-1/3 bg-white z-10'>
-            <div className='m-6 flex justify-between'>
-              <p className='text-4xl font-display'>Cart <span className='text-lg'>(0 items)</span></p>
-              {/* <svg onClick={() => setOpen('invisible')} className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg> */}
-            </div>
-            <div className='absolute font-body inset-y-1/2 inset-x-1/3 w-full h-24'>
-              <p>Your cart is empty!</p>
-              <button className='h-12 p-4 my-4 w-1/3 font-body text-sm bg-gray-900 text-white rounded-r transition duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'>
-              Shop Now</button>          
-            </div>
+    <div className={display}>
+      <div className='overflow-hidden w-screen flex'>
+        <div 
+          className='absolute w-2/3 inset-0 bg-gray-700 bg-opacity-50 h-screen z-20'
+          onClick={() => dispatch(hideCart())}
+        >
+        </div>
+        <div className='absolute text-black inset-y-0 right-0 h-screen w-1/3 bg-white z-10'>
+          <div className='m-6 flex justify-between'>
+            <p className='text-4xl font-display'>Cart <span className='text-lg'>(0 items)</span></p>
+              <svg 
+                onClick={() => dispatch(hideCart())} 
+                className="h-6 w-6" 
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" 
+              />
+            </svg>
+          </div>
+          <div className='absolute font-body inset-y-1/2 inset-x-1/3 w-full h-24'>
+            <p>Your cart is empty!</p>
+            <button className='h-12 p-4 my-4 w-1/3 font-body text-sm bg-gray-900 text-white rounded-r transition duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'>
+            Shop Now</button>          
           </div>
         </div>
       </div>
@@ -25,3 +37,5 @@ const Cart = () => {
 };
 
 export default Cart;
+
+// onClick={() => setDisplayCart('hidden')} 
--- src/components/Cart/cartSlice.js
+++ src/components/Cart/cartSlice.js
@@ -1,20 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit'
-import products from '../Shop/ItemData'
+import products from '../../products/products'
 
 export const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         products: products,
         addedIds: [],
-        quantityById: {}
+        quantityById: {},
+        display: 'hidden'
     },
     reducers: {
         addToCart: (state, action) => {
             state.addedIds.push(action.payload)
+        },
+        displayCart: (state) => {
+            state.display = null
+        },
+        hideCart: (state) => {
+            state.display = 'hidden'
         }
     }
 })
 
-export const { addToCart } = cartSlice.actions
+export const { addToCart, displayCart, hideCart} = cartSlice.actions
 
 export default cartSlice.reducer
