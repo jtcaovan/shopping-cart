176dc016b605adb555acaa2ba9c98b34d7b5c07a
Justin Caovan <jtcaovan@gmail.com>
2021-05-26 21:05:05 -0700

    Added empty state conditional rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,8 +9,6 @@ const Cart = () => {
   const total = useSelector((state) => state.cart.total)
   const dispatch = useDispatch()
 
-  // let cartIds = cart.find(id => id === products.id)
-
   let cartItems = cart.map(item => 
     <CartDisplay 
         id={item.product.id}
@@ -23,12 +21,15 @@ const Cart = () => {
 
   return (
     <div className={display}>
+
       <div className='overflow-hidden w-screen flex'>
+
         <div 
           className='absolute w-2/3 inset-0 bg-gray-700 bg-opacity-50 h-screen z-20'
           onClick={() => dispatch(hideCart())}
         >
         </div>
+
         <div className='absolute text-black inset-y-0 right-0 h-screen w-1/3 bg-white z-10'>
           <div className='m-6 flex justify-between'>
             <p className='text-4xl font-display'>Cart <span className='text-lg'>({total} items)</span></p>
@@ -41,20 +42,22 @@ const Cart = () => {
             </svg>
           </div>
 
-          {/* If cart = 0 ? display empty state : CartDisplay */}
-          <div className=' font-body inset-y-1/2 inset-x-1/3 w-full h-24'>
-            {/* <p>Your cart is empty!</p>
-            <button 
-              className='h-12 p-4 my-4 w-1/3 font-body text-sm bg-gray-900 text-white rounded-r transition 
-              duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'
-            >
-            Shop Now
-            </button>    */}
-
-            {cartItems}
-          <button>
+          <div className=''>
+                {total === 0 &&   
+                  <div className='font-body absolute inset-y-1/2 inset-x-1/3 w-full h-24'>
+                    <p>Your cart is empty!</p>
+                    <button 
+                      className='h-12 p-4 my-4 w-1/3 font-body text-sm bg-gray-900 text-white rounded-r transition 
+                      duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'
+                    >
+                    Shop Now  
+                    </button>
+                </div> }
+                {cartItems}
+{/* 
+            <button>
                 Checkout
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
--- src/components/Cart/CartDisplay.js
+++ src/components/Cart/CartDisplay.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 function CartDisplay(props) {
     return (
-        <div className='relative flex space-x-4'>
+        <div className='flex justify-between m-6'>
             <img 
                 className='h-12 w-12'
-                src={props.img} alt=''/>
-            <div className='flex justify-between'>
-                <p>{props.id}</p>
-                <p>{props.name}</p>
-                <p>{props.price}</p>
-                <p>{props.quantity}</p>
-            </div>
+                src={props.img} alt=''
+            />
+            <p>{props.name}</p>
+            <p>{props.quantity}</p>
+            <p>{props.price}</p>
         </div>
     )
 }
