94ddda729f0f716f9ae7504b4e2ef2959108d609
Justin Caovan <jtcaovan@gmail.com>
2021-05-27 22:09:02 -0700

    Code spacing

--- src/components/Nav/ItemNav.js
+++ src/components/Nav/ItemNav.js
@@ -5,6 +5,7 @@ import { displayCart } from '../Cart/cartSlice'
 
 const ItemNav = () => {
     const dispatch = useDispatch()
+    
   return (
         <nav className='flex h-18 py-4 w-full m-auto text-gray-800'> 
             <Link to='/'>
