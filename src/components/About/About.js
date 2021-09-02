c01bba594113009bcb11bd6371ba5b722900959e
Justin Caovan <jtcaovan@gmail.com>
2021-09-02 15:36:33 -0700

    Mobile responsive changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,8 +15,8 @@ const About = () => {
       <AboutHero />
       <div className=" text-gray-700 bg-white min-h-0 h-auto">
         <div className='w-9/12 text-center m-auto'>
-            <h1 className='font-display text-4xl m-12'>Our Humble Beginnings</h1>
-            <div className='font-body leading-loose text-center flex flex-col w-full 2xl:w-1/2 2xl:m-auto space-y-6'>
+            <h1 className='font-display text-2xl md:text-4xl m-12'>Our Humble Beginnings</h1>
+            <div className='font-body leading-loose text-sm md:text-base text-center flex flex-col w-full 2xl:w-1/2 2xl:m-auto space-y-6'>
               <p>
               In 2017, Justin Caovan lived in a small, boring apartment in Long Beach, CA while attending university.
               After a couple of years of sharing a room, he finally had his own space and decided to spruce it up and make it his own. At a local swapmeet Justin picked up several houseplants
@@ -30,12 +30,12 @@ const About = () => {
               </p>
             </div>
 
-            <h2 className='font-display text-4xl m-12'>Get in Contact!</h2>
-            <div className='flex w-full 2xl:w-3/4 2xl:m-auto mb-20 font-body leading-loose space-x-12 justify-between'>
-              <div className='w-7/12'>
+            <h2 className='font-display text-2xl md:text-4xl m-12'>Get in Contact!</h2>
+            <div className='flex flex-col md:flex-row w-full 2xl:w-3/4 m-auto mb-20 font-body leading-loose space-y-6 md:space-y-0 md:space-x-12 justify-between'>
+              <div className='w-full md:w-7/12'>
                 <img src={aboutImg} alt='Multiple plants'/>
               </div>
-              <div className='w-5/12 flex-col space-y-5 m-auto text-left font-display'>
+              <div className='w-full md:w-5/12 flex flex-col space-y-5 m-auto text-left font-display text-sm md:text-base leading-loose'>
                 <p >Questions, comments, concerns? Bloom is always looking for new opportunities, local artisans, and ways to spread our love of plants!</p>
                 <p>Email us! <br></br>jtcaovan@gmail.com</p>
                 <div className='flex space-x-6'>
