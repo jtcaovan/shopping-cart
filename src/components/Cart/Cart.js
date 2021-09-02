5df8b48140242b4b939f243f66c62de29e2f3d22
Justin Caovan <jtcaovan@gmail.com>
2021-09-02 16:03:16 -0700

    Cart now mobile responsive

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -25,17 +25,13 @@ const Cart = () => {
   return (
     <div className={display}>
       <div className='z-10 fixed w-full h-full'>
-        <div 
-          className='absolute w-3/4 inset-0 bg-gray-700 bg-opacity-50  z-20'
-          onClick={() => dispatch(hideCart())}
-        >
+        <div className='absolute w-1/4 md:w-3/4 inset-0 bg-gray-700 bg-opacity-50 z-20'
+          onClick={() => dispatch(hideCart())}>
         </div>
-
-        <div className='absolute inset-y-0 right-0 w-1/4 bg-white z-10'>
+        <div className='absolute inset-y-0 right-0 w-3/4 md:w-1/4 bg-white z-10'>
           <div className='flex justify-between h-16 bg-gray-200 bg-opacity-90'>
             <p className='text-3xl mx-4 my-3 text-gray-700 font-display'>Cart <span className='text-lg'>({totalItems} items)</span></p>
-              <svg 
-                onClick={() => dispatch(hideCart())} 
+              <svg onClick={() => dispatch(hideCart())} 
                 className="m-4 h-6 w-6 cursor-pointer text-gray-700 hover:text-gray-900" 
                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" 
@@ -43,14 +39,13 @@ const Cart = () => {
             </svg>
           </div>
 
-          <div >
+          <div className='h-full'>
             {totalItems === 0 &&   
-              <div className='font-body absolute inset-y-1/2 inset-x-1/3 w-full h-24'>
+              <div className='font-body flex flex-col justify-center items-center my-auto w-full h-3/4 md:h-full'>
                 <p>Your cart is empty!</p>
                 <Link to='/shop'>
-                  <button
-                    onClick={() => dispatch(hideCart())}
-                    className='h-12 p-4 my-4 w-1/3 font-body text-sm bg-gray-900 text-white rounded transition 
+                  <button onClick={() => dispatch(hideCart())}
+                    className='h-12 p-4 my-4 w-full text-sm bg-gray-900 text-white rounded transition 
                     duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'
                   >
                   Shop Now  
@@ -71,7 +66,7 @@ const Cart = () => {
               </div>
               <div className='text-center'> 
                 <button   
-                className='h-12 p-4 w-2/4 font-body text-sm bg-gray-900 text-white rounded transition 
+                className='h-12 p-4 w-2/4 text-sm bg-gray-900 text-white rounded transition 
                   duration-300 border-gray-900 hover:bg-transparent hover:text-gray-900 hover:border'>
                     Checkout
                 </button>
